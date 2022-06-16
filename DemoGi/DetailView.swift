3e9b5562bcc2904c7bd3ba4957345d87ea07a901
LoopingStudio <kelian.daste31@gmail.com>
2022-06-16 09:55:22 +0200

    change detailview path, and content

--- DemoGi/DetailView.swift
+++ DemoGi/DetailView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Texteu")
+            Image(systemName: "sun.fill.max")
+        }
     }
 }
 
