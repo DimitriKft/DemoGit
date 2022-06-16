28304ad4b33998f03cf912ed492772277b567beb
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-06-16 09:37:52 +0200

    first commit

---
+++ DemoGi/DemoGiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGiApp.swift
+//  DemoGi
+//
+//  Created by dimitri on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoGiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
